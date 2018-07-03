9753ab68b66bd537b361ffe374263973c319cf45
Acun <acun1993@gmail.com>
2018-07-03 11:12:44 +0300

    Initial commit

---
+++ SQLScheduler/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SQLScheduler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listDatabase = new System.Windows.Forms.ListView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listDatabase
+            // 
+            this.listDatabase.Location = new System.Drawing.Point(12, 33);
+            this.listDatabase.Name = "listDatabase";
+            this.listDatabase.Size = new System.Drawing.Size(776, 97);
+            this.listDatabase.TabIndex = 0;
+            this.listDatabase.UseCompatibleStateImageBehavior = false;
+            
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Databases:";
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(359, 136);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 4;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 176);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listDatabase);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listDatabase;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button connectButton;
+    }
+}
+
---
+++ SQLScheduler/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SQLScheduler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string connectionString = "Data Source='DESKTOP-HPQ1QQU\\ACUN'; Integrated Security=True;";
+            List<string> databaseNames = new List<string>();
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                try{
+                    sqlConnection.Open();
+                }
+                catch (SqlException ex) {
+                    MessageBox.Show("Sql connection error", "Error = " + ex.Message, MessageBoxButtons.OK);
+                }
+
+                 using (SqlCommand cmd = new SqlCommand("SELECT * FROM sys.databases WHERE database_id > 4", sqlConnection))
+                 {
+
+                    SqlDataReader sqlDataReader = null;
+                    
+                    try
+                     {
+                         sqlDataReader = cmd.ExecuteReader();
+                     }
+                     catch (InvalidOperationException ex) {
+                         MessageBox.Show("SQL Reader error.", "Error = " + ex.Message, MessageBoxButtons.OK);
+                     }
+
+                     while (sqlDataReader.Read())
+                     {
+
+                        Console.WriteLine(sqlDataReader[0].ToString());
+
+                        ListViewItem item = new ListViewItem(sqlDataReader[0].ToString());
+                        listDatabase.Items.Add(item);
+                     
+                     }
+                 }
+              
+            }
+
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("deneme", "deneme baslik?", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+    }
+}
